8d6eedccf6b4829e786a0e7c01acbb279de45dd2
fadi dasus <fadidasus@gmail.com>
2025-08-26 10:30:38 +0200

    remove stuff

--- AKS-Application-Gateway-WAF/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "kube_config_raw" {
-  description = "The `azurerm_kubernetes_cluster`'s `kube_config_raw` argument. Raw Kubernetes config to be used by [kubectl](https://kubernetes.io/docs/reference/kubectl/overview/) and other compatible tools."
-  sensitive   = true
-  value       = azurerm_kubernetes_cluster.aks-cluster.kube_config_raw
-}
-
-output "kube_host" {
-  value = nonsensitive(azurerm_kubernetes_cluster.aks-cluster.kube_config[0].host)
-}
